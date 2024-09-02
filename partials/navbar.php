1e8665bfcccbe7a696adf91c6779d577f2dd452f
Vishal Tiwari <vishaltiwari909080@gmail.com>
2024-09-02 14:03:13 +0530

    created pages and signup form

--- partials/navbar.php
+++ partials/navbar.php
@@ -12,21 +12,11 @@
             <li class="nav-item">
               <a class="nav-link" href="/loginSystem/login.php">Login</a>
             </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                Dropdown
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li>
-                  <hr class="dropdown-divider">
-                </li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>
+            <li class="nav-item">
+              <a class="nav-link" href="/loginSystem/signup.php">SignUp</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              <a class="nav-link" href="/loginSystem/logout.php">LogOut</a>
             </li>
           </ul>
           <form class="d-flex" role="search">
