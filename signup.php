aacc626dd9c096ee52faa0ab49a423f0bd4bd569
Vishal Tiwari <vishaltiwari909080@gmail.com>
2024-09-04 14:26:47 +0530

    signup page completed

--- signup.php
+++ signup.php
@@ -1,3 +1,12 @@
+<?php
+  $server = "localhost";
+  $username="root";
+  $password="";
+  $database="cust_data";
+  $connect = mysqli_connect($server,$username,$password,$database);
+  global $value;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,26 +17,109 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div class="container">
-        <?php
-            require "./partials/navbar.php";
-        ?>
-    </div>
+  <div class="container">
+    <?php
+      require "./partials/navbar.php";
+    ?>
+  </div>
+
+  <div class="container my-5">
+    <?php
+      if(!$connect){
+        echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                <strong>Sorry!</strong> We regret we are facing some technical issues, we fix if ASAP!.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>';
+        exit;
+  }
+
+  else{
+
+    if($_SERVER['REQUEST_METHOD']=='POST'){
+
+      $email=$_POST['email'];
+      $pass=$_POST['password'];
+      $cpass=$_POST['cpassword'];
+
+      if ($pass==$cpass) {
+
+        if ( $email!="" && $pass!="") { 
+
+          $sql="insert into data (Email,Password) values ('".$email."','".$pass."')";
+          try {
+            $result=mysqli_query($connect,$sql);
+            $value="";
+            if ($result) {
+
+              echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Success!</strong> You are now signedup, Go to login page.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>';
+              $value="";
+            }
+            else{
+
+              echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                      <strong>Sorry!</strong> We regret we are facing some technical issues, we fix if ASAP!.
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>';
+
+            }
+          }  
+          catch(mysqli_sql_exception){
+
+            echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                    <strong>Duplicate Entry!</strong> This email already exist, Please login or try with another email.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                  </div>';
+              $value="";
+            }
+        }
+        else {
+          echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+          <strong>Warning!</strong> Input fields can not be empty, Please fill properly.
+          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
+        }
+
+      }
+      else{
+
+        echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Unmatched!</strong> The password you entered is not correctly matched with confirm password.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+              </div>';
+
+      }
+
+
+    }
+
+    else{
+      echo '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+                      <strong>Sorry!</strong> We regret we are facing some technical issues, we fix if ASAP!.
+                      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>';
+    }
+    
+  }
+    ?>
+  </div>
     
   <div class="container my-5 ">
-    <form action="/loginSystem/signup.php" method="post">
+    <form action="/loginSystem/signup.php" method="POST">
       <div class="mb-3">
         <label for="email" class="form-label">Email address</label>
-        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp" value="<?php echo ($value); ?>">
         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
       </div>
       <div class="mb-3">
         <label for="password" class="form-label">Password</label>
-        <input type="password" class="form-control" id="password" name="password">
+        <input type="password" class="form-control" id="password" name="password" value="<?php echo ($value); ?>">
       </div>
       <div class="mb-3">
         <label for="cpassword" class="form-label">Confirm Password</label>
-        <input type="password" class="form-control" id="cpassword">
+        <input type="password" class="form-control" id="cpassword" name="cpassword">
       </div>
       
       <button type="submit" class="btn btn-primary">Submit</button>
